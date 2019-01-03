899a3391bf3442ae8fc49bf96088c736dc19b483
Raul Schmidlin <raul.schmidlin@schneider-electric.com>
2019-01-03 12:27:30 +0100

    imenu bound to <C-,>; smart beginning of line; indexer configuration

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
+;; Initialize proxy configuration
+(load-file "~/.emacs.d/proxy_conf.el")
+
 ;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -15,7 +18,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (boon which-key use-package smex flx counsel))))
+ '(package-selected-packages
+   (quote
+	(ggtags projectile realgud boon which-key use-package smex flx counsel)))
+ '(safe-local-variable-values
+   (quote
+	((company-clang-arguments "-target" "i686-pc-windows-gnu" "-include" "c:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Workspace/UnitTestsCMake/UnitTestGlobal.h" "-Ic:/mingw/lib/gcc/mingw32/6.3.0/include/c++" "-Ic:/mingw/lib/gcc/mingw32/6.3.0/include/c++/mingw32" "-Ic:/mingw/lib/gcc/mingw32/6.3.0/include/c++/backward" "-Ic:/mingw/lib/gcc/mingw32/6.3.0/include" "-Ic:/mingw/include" "-Ic:/mingw/lib/gcc/mingw32/6.3.0/include-fixed" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/ExternalSources" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/ExternalSources/CoSeMa" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/GeneratedSources/Cds/Components" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/AdaptionLayer" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/ExternalSources" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/Manager" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel/Import/sercoshwmanager/Sources/Platform" "-IC:/pcrt_ext_motionkernel_ci/pcrt_ext_motionkernel")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
